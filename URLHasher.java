96a6163f6000f993e1d5a2d8b4a71ff8cbf6878d
Soumya130498 <113308735+Soumya130498@users.noreply.github.com>
2023-11-23 11:48:04 -0500

    Update URLHasher.java

--- URLHasher.java
+++ URLHasher.java
@@ -1,1 +1,37 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class URLHasher {
+
+    public static String hashURL(String url) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+
+            byte[] hashBytes = digest.digest(url.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte hashByte : hashBytes) {
+                String hex = Integer.toHexString(0xff & hashByte);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        String url = "https://wordpress.com/go/content-blogging/bloggers-block-writing-resources-to-try-when-youre-out-of-ideas/";
+        String hashedURL = hashURL(url);
+
+        System.out.println("Original URL: " + url);
+        System.out.println("Hashed URL: " + hashedURL);
+    }
+}
 
